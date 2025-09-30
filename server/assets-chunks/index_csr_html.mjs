c0dab7cc5c2bce2ed9c2a47f16b315c1a3cdb0f1
Andro <androixus@yahoo.com>
2025-10-01 01:38:49 +0300

    Auto-generated commit

--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -3,7 +3,7 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>GameRent</title>
-  <base href="https://github.com/androixus/calculate_your_share.git">
+  <base href="https://androixus.github.io/calculate_your_share/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
